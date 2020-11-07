c30808110a4c48684145825283e3b10de1ac4be2
Ryan Beiden <rdbeiden@gmail.com>
2020-11-07 14:55:06 -0500

    add link from all posts page to a single post

--- src/components/Posts/post.js
+++ src/components/Posts/post.js
@@ -9,6 +9,7 @@ class Post extends React.Component {
   render() {
     const { post } = this.props;
     const filterDate = moment(post.publication_date).format('MMMM Do YYYY, hh:mm:ss a');
+    const view_post = `posts/${post.id}`
     // if you want only month.day.year
     // moment().format('MM-DD-YYYY')
     // style="width: 18rem;"
@@ -16,7 +17,7 @@ class Post extends React.Component {
       <div className="card">
         <image src={post.image_url} class="card-img-top" alt="post image"></image>
         <div className="card-body">
-          <h5 className="card-title">{post.title}</h5>
+          <Link to={view_post}><h5 className="card-title">{post.title}</h5></Link>
           <p className="card-text">{post.full_name} .. {filterDate}</p>
           <p className="card-text">{post.category_name}</p>
           <Link className="btn btn-primary" to="/posts/new">Comment</Link>
