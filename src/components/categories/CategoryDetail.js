fc48da5fdae2b94e823c18b44239a61f4b20c946
Jeanine Beckle <jeaninebeckle@gmail.com>
2021-01-05 19:52:53 -0600

    displays all posts belonging to a category

--- src/components/categories/CategoryDetail.js
+++ src/components/categories/CategoryDetail.js
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 import { CategoryContext } from "./CategoryProvider"
 import "./Categories.css"
+
 
 export const CategoryDetails = (props) => {
     const { deleteCategory, getCategoryById } = useContext(CategoryContext)
@@ -16,7 +18,7 @@ export const CategoryDetails = (props) => {
 
     return (
         <section className="category">
-            <h3 className="category__name">{categories.category_name}</h3>
+            <h3 className="category__name">{categories.label}</h3>
 
             <button onClick={() => window.confirm('Are you sure?') &&
             deleteCategory(categories.id).then(() => props.history.push("/categories"))
@@ -25,6 +27,23 @@ export const CategoryDetails = (props) => {
             <button onClick={() => {
                 props.history.push(`/categories/edit/${categories.id}`)
             }}>Edit</button>
+
+            <h3>Posts</h3>
+            {
+                categories.posts&&
+                    categories.posts.map(post => {
+                        return <section key={`post--${post.id}`} className="category-posts">
+                            <div className="card">
+                                <div className="card-body">
+                                <Link to={`/posts/${post.id}`}><h3 className="card-title">{post.title}</h3></Link>
+                                <p className="card-text">{post.content}</p>
+                                <p className="card-text" style={{ fontSize: ".9em" }}></p>
+                                </div>
+                            </div>
+                        </section>
+                    })
+            }
+
         </section>
     )
 }
