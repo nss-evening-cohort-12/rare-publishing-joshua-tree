34e1eb0e2611a8cc37a42e808685e90b07ee7760
Ryan Beiden <rdbeiden@gmail.com>
2021-01-16 11:05:19 -0600

    wip

--- src/components/Users/SingleUser.js
+++ src/components/Users/SingleUser.js
@@ -6,23 +6,39 @@ class SingleUser extends React.Component {
 
   state = {
     user: {},
-    rare_user: {}
+    rare_user: {},
+    subscription: []
   }
 
   componentDidMount() {
     const { userId } = this.props.match.params;
+    const authorId = parseInt(userId);
+    const followerId = parseInt(localStorage.getItem("rare_user_id"));
     
     UserProvideComp.getUserById(userId)
       .then((response) => this.setState({
         user: response,
-        rare_user :response.user,
-
+        rare_user: response.user,
       }))
+    
+    UserProvideComp.getSingleSub(authorId, followerId)
+      .then((response) => {
+        this.setState({ subscription: response })
+      })
+  }
 
-  }  
+  subscribeEvent = () => {
+    const { subscription } = this.state;
+
+    if (subscription.created_on > subscription.ended_on) {
+      console.log('Update subscription!')
+    } else if (subscription.length < 1) {
+      console.log('Create subscription!')
+    }
+  }
 
   render() {
-    const { user, rare_user } = this.state;
+    const { user, rare_user, subscription } = this.state;
     return (
       <div className="container">
         <img alt="no-img" src="https://fishingbakersfield.com/icons/user.svg" className="user_image"/>
@@ -33,6 +49,9 @@ class SingleUser extends React.Component {
           <h2 className="content btn-info">{rare_user.date_joined}</h2>
           <h2 className="content btn-info">Is staff {rare_user.is_staff}</h2>
         </div>
+        <button className="subscribe-button" onClick={this.subscribeEvent}>
+          {subscription.created_on > subscription.ended_on || subscription.length >= 1 ? 'Unsubscribe' : 'Subscribe'}
+        </button>
       </div>
     );
   }
